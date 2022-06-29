c49e7e0c2cb24fb9757e91958bfa6777dac10268
Eduardo <eduardosantana1313@gmail.com>
2022-06-29 13:28:59 -0300

    Adding configs

--- greeting-service/src/main/java/br/com/edu/greeting/service/controllers/GreetingController.java
+++ greeting-service/src/main/java/br/com/edu/greeting/service/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package br.com.edu.greeting.service.controllers;
 
+import br.com.edu.greeting.service.configuration.GreetingConfiguration;
 import br.com.edu.greeting.service.services.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,17 +11,25 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-
+    private static final String template = "%s, %s!";
     private final AtomicLong counter = new AtomicLong();
+
+    private final GreetingConfiguration configuration;
+
+    public GreetingController(GreetingConfiguration configuration) {
+        this.configuration = configuration;
+    }
 
     @RequestMapping("/greeting")
     public Greeting greeting(
             @RequestParam(value="name",
-                    defaultValue = "World") String name) {
+                    defaultValue = "") String name) {
+
+        if (name.isEmpty()) name = configuration.getDefaultValue();
+
         return new Greeting(
                 counter.incrementAndGet(),
-                String.format(template, name)
+                String.format(template, configuration.getGreeting(), name)
         );
     }
 
