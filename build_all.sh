b006cbfe3ceb1eced82cb0e280d13303da7f39a4
Eduardo <eduardosantana1313@gmail.com>
2022-07-05 19:14:35 -0300

    adding projects to build

--- build_all.sh
+++ build_all.sh
@@ -3,3 +3,10 @@ mvn spring-boot:build-image -DskipTests
 
 cd ../book-service
 mvn spring-boot:build-image -DskipTests
+
+cd ../cambio-service
+mvn spring-boot:build-image -DskipTests
+
+cd ../naming-service
+mvn spring-boot:build-image -DskipTests
+
